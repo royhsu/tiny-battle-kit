3d8e8495c0d42cbe8c1ce54457d9b15b30b7e821
Roy Hsu <roy.hsu@tinyworld.cc>
2017-11-30 22:09:57 +0800

    add xcode project

---
+++ TinyBattleKit/TinyBattleKit.h
@@ -0,0 +1,19 @@
+//
+//  TinyBattleKit.h
+//  TinyBattleKit
+//
+//  Created by Roy Hsu on 30/11/2017.
+//  Copyright © 2017 TinyWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyBattleKit.
+FOUNDATION_EXPORT double TinyBattleKitVersionNumber;
+
+//! Project version string for TinyBattleKit.
+FOUNDATION_EXPORT const unsigned char TinyBattleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyBattleKit/PublicHeader.h>
+
+
